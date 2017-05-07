5bb0a3f1502502f5b9c3a4d787ddf25782ce3913
sThig <scott.thigpen@gmail.com>
2017-05-07 14:36:29 -0500

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCDrBHxCsFSvUh-XqC5SYzSj1UO6FBkjQ0",
+  authDomain: "catch-of-the-day-scott-168cd.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-scott-168cd.firebaseio.com",
+});
+
+export default base;
